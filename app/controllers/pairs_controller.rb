6c886cf4b592a51083f400977166ec73a7b7a2cc
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-11 11:15:46 +0900

    improve:pairデータupdate時の処理・enum国際化を追加#7

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -18,16 +18,6 @@ class PairsController < ApplicationController
     elsif @pair.save
       @pair.assigns.create(user: pertner)
       @pair.assigns.create(user: current_user)
-      @pair.tasks.create(user: current_user, title:"衣装", description:"ウェディングドレス・和装の決定・アクセサリーやシューズ等の小物の決定",expired_on: @pair.weddingday_on<<4, status: 0)
-      @pair.tasks.create(user: current_user, title:"料理・ドリンク", description:"料理・ドリンクコースの内容の決定",expired_on: @pair.weddingday_on<<2, status: 0)
-      @pair.tasks.create(user: current_user, title:"引出物・引菓子", description:"引出物・引菓子・プチギフトの選定",expired_on: @pair.weddingday_on<<2, status: 0)
-      @pair.tasks.create(user: current_user, title:"招待状", description:"招待客の最終決定と招待状の作成・発送",expired_on: @pair.weddingday_on<<3, status: 0)
-      @pair.tasks.create(user: current_user, title:"前撮り", description:"前撮りの検討・スタジオやロケーションの決定",expired_on: @pair.weddingday_on<<4, status: 0)
-      @pair.tasks.create(user: current_user, title:"装花", description:"会場コーディネートと装花の決定",expired_on: @pair.weddingday_on<<3, status: 0)
-      @pair.tasks.create(user: current_user, title:"演出", description:"当日のプログラムと演出の決定",expired_on: @pair.weddingday_on<<3, status: 0)
-      @pair.tasks.create(user: current_user, title:"写真・映像・BGM", description:"結婚式当日に使用する写真・映像・BGM等の選定・作成",expired_on: @pair.weddingday_on<<2, status: 0)
-      @pair.tasks.create(user: current_user, title:"最終打合せ", description:"結婚式場での最終打合せ",expired_on: @pair.weddingday_on<<1, status: 0)
-      @pair.tasks.create(user: current_user, title:"席次表の提出", description:"席次表の提出",expired_on: @pair.weddingday_on<<1, status: 0)
       redirect_to pair_path(@pair)
     else
       render :new
@@ -39,7 +29,8 @@ class PairsController < ApplicationController
 
   def update
     if @pair.update(pair_params)
-       #@pair.tasks.destroy_all
+       @pair.tasks.destroy_all
+       @pair.create_default_tasks
        redirect_to pair_path(@pair)
     else
       render :new
