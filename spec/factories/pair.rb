898d2333d49ca8619824a425840021ea76e2ab83
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-14 16:11:29 +0900

    add:ペア作成後のタスク一覧表示のRSpec追加#6

--- spec/factories/pair.rb
+++ spec/factories/pair.rb
@@ -1,6 +1,6 @@
-# FactoryBot.define do
-#   factory :pair do
-#     weddingday_on { '2022/12/10'}
-#     season  { :winter }
-#   end
-# end
+FactoryBot.define do
+  factory :pair do
+    weddingday_on { '2022/12/10'}
+    season  { :winter }
+  end
+end
