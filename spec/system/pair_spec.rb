b6a027d35b4c87275280186784248516da8c06a8
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-14 11:19:02 +0900

    add:ペア機能作成のRSpec追加#6

---
+++ spec/system/pair_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+
+RSpec.describe Pair, type: :system do
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:user_second) {FactoryBot.create(:user_second)}
+  before do
+    sign_in user
+  end
+  describe 'ペア作成機能' do
+    context 'タスク作成ボタンを押した場合' do
+      it 'ペア作成ページに遷移する' do
+        visit posts_path
+        click_on "タスク作成"
+        expect(current_path).to eq new_pair_path
+        expect(page).to have_content "挙式情報や招待するお相手を入力してTODOリストを作りましょう"
+      end
+    end
+    context '招待する相手のメールアドレスを正しく入力した場合' do
+      it 'ペアが作成される' do
+        visit new_pair_path
+        select '2022', from: "pair_weddingday_on_1i"
+        select '12', from: "pair_weddingday_on_2i"
+        select '10', from: "pair_weddingday_on_3i"
+        select '冬', from: "pair_season"
+        fill_in "pertner_email", with: user_second[:email]
+        click_on "commit"
+        expect(page).to have_content user[:name]
+        expect(page).to have_content user_second[:name]
+      end
+    end
+    context '既にペア作成されているユーザーやユーザー登録されていないメールアドレスを入力した場合' do
+      it 'エラーメッセージが表示される' do
+        visit  new_pair_path
+        select '2022', from: "pair_weddingday_on_1i"
+        select '12', from: "pair_weddingday_on_2i"
+        select '10', from: "pair_weddingday_on_3i"
+        select '冬', from: "pair_season"
+        fill_in "pertner_email", with: 'yyyy@gmail.com'
+        click_on "commit"
+        expect(page).to have_content "メールアドレスが正しくありません"
+      end
+    end
+    context 'ペアの作成が完了した場合' do
+      it 'ペア詳細ページに遷移しデフォルトのTODOリスト一覧が表示される' do
+        visit new_pair_path
+        select '2022', from: "pair_weddingday_on_1i"
+        select '12', from: "pair_weddingday_on_2i"
+        select '10', from: "pair_weddingday_on_3i"
+        select '冬', from: "pair_season"
+        fill_in "pertner_email", with: user_second[:email]
+        click_on "commit"
+        expect(page).to have_content "衣装"
+        expect(page).to have_content "前撮り"
+      end
+    end
+  end
+end
