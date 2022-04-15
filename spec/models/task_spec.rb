134dc95c19113204577f9db4462b03306416edfe
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-15 13:59:26 +0900

    add:タスクモデルの成功パターンのRSpecを追加#7

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+  let!(:assign) {FactoryBot.create(:assign, user_id: user.id, pair_id: pair.id)}
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションに引っかかる' do
@@ -18,6 +21,12 @@ RSpec.describe Task, type: :model do
       it 'バリデーションに引っかかる' do
         task = Task.new(title: '失敗テスト', description:'')
         expect(task).not_to  be_valid
+      end
+    end
+    context 'タスクのタイトルと詳細が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = Task.new(title: '成功テスト', description: '成功テスト', user: user, pair: pair)
+        expect(task).to be_valid
       end
     end
   end
