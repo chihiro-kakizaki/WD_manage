9c13ef73fc2a2816bb4a00456db732ebcdbec5af
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-15 16:24:53 +0900

    add:userモデルにアソシエーションのRSpec追加#24

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,4 +45,43 @@ RSpec.describe User, type: :model do
       end
     end
   end
+  describe 'アソシエーションテスト' do
+    let!(:user) {FactoryBot.create(:user)}
+    context 'ユーザーが削除された場合' do
+      let!(:post) {FactoryBot.create(:post, user_id: user.id)}
+      let!(:favorite) {FactoryBot.create(:favorite, user: user, post: post )}
+      it '該当ユーザーのfavoriteデータも削除される' do
+        expect { user.destroy }.to change { Favorite.count }.by(-1)
+      end
+    end
+    context 'ユーザーが削除された場合' do
+      let!(:post) {FactoryBot.create(:post, user_id: user.id)}      
+      it '該当ユーザーのpostデータも削除される' do
+        expect { user.destroy }.to change { Post.count }.by(-1)
+      end
+    end
+    context 'ユーザーが削除された場合' do
+      let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+      it '該当ユーザーのtaskデータも削除される' do
+        #pairがsaveされた時にcreate_default_tasksによりtaskが8個生成される
+        expect { user.destroy }.to change { Task.count }.by(-8)
+      end
+    end
+    context 'ユーザーが削除された場合' do
+      let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+      it '該当ユーザーのpairデータも削除される' do
+        expect { user.destroy }.to change { Pair.count }.by(-1)
+      end
+    end
+    context 'ユーザーが削除された場合' do
+      let!(:user_second) {FactoryBot.create(:user_second)}
+      let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+      let!(:assign) {FactoryBot.create(:assign, user_id: user.id, pair_id: pair.id)}
+      let!(:assign2) {FactoryBot.create(:assign, user_id: user_second.id, pair_id: pair.id)}
+      it '該当ユーザーのassignデータも削除される' do
+        #2人ペアなのでどちらか一方のユーザーが削除された時点でassignは-2の結果が期待される
+        expect { user.destroy }.to change { Assign.count }.by(-2)
+      end
+    end
+  end
 end
