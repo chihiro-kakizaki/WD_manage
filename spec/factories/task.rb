597ac1e0e6cf28a1bec33c89f3863c3f7299a0ad
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-15 10:54:16 +0900

    add:TODOリスト機能の一覧画面のRspec追加#7

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :task do
     title { 'テストタイトル' }
     description { 'テスト詳細' }
-    expired_on { '2022/8/10' }
+    expired_on { '2022/5/10' }
     status { :not_started }
   end
-  factory :second_task, class: Task do
+  factory :task_second, class: Task do
     title { '卒業課題' }
     description { 'WD_manage' }
     expired_on { '2022/6/10' }
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:user_second) {FactoryBot.create(:user_second)}
+  let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+  let!(:assign) {FactoryBot.create(:assign, user_id: user.id, pair_id: pair.id)}
+  let!(:assign2) {FactoryBot.create(:assign, user_id: user_second.id, pair_id: pair.id)}
+  let!(:task) {FactoryBot.create(:task, user_id: user_second.id, pair_id: pair.id)}
+  let!(:task_second) {FactoryBot.create(:task_second, user_id: user_second.id, pair_id: pair.id)}
+  before do
+    sign_in user
+  end
   describe '新規作成機能' do
-    let!(:user) {FactoryBot.create(:user)}
-    let!(:user_second) {FactoryBot.create(:user_second)}
-    let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
-    let!(:assign) {FactoryBot.create(:assign, user_id: user.id, pair_id: pair.id)}
-    let!(:assign2) {FactoryBot.create(:assign, user_id: user_second.id, pair_id: pair.id)}
-    before do
-      sign_in user
-    end
     context 'タスクの新規作成ボタンを押した場合' do
       it 'タスク作成ページに遷移する' do
         visit pair_path(pair)
@@ -42,7 +43,8 @@ RSpec.describe 'タスク管理機能', type: :system do
         select '未着手', from: "task_status"
         click_on "commit"
         expect(current_path).to eq pair_path(pair)
-        expect(page).to have_content '打ち合わせ'
+        task_list = all('.task_row')
+        expect(task_list.last).to have_content '打ち合わせ'
       end
     end
     context 'タイトルと詳細を入力せずに作成ボタンを押した場合' do
@@ -57,6 +59,48 @@ RSpec.describe 'タスク管理機能', type: :system do
         click_on "commit"
         expect(page).to have_content "タイトルを入力してください"
         expect(page).to have_content "詳細を入力してください"
+      end
+    end
+  end
+
+  describe '一覧表示機能' do
+    context 'タスク一覧画面ボタンを押した場合' do
+      it 'ペア詳細ページに遷移する' do
+        visit posts_path
+        click_on "タスク一覧画面"
+        expect(current_path).to eq pair_path(pair)
+      end
+    end
+    context 'ペア詳細ページに遷移した場合' do
+      it 'ペア詳細情報が表示される' do
+        visit pair_path(pair)
+        expect(page).to have_content pair.weddingday_on
+        expect(page).to have_content user.name
+        expect(page).to have_content user_second.email
+      end
+    end
+    context 'ペア詳細ページに遷移した場合' do
+      it 'タスク一覧が作成した日時の降順で表示される' do
+        visit pair_path(pair)
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content "衣装"
+        expect(task_list.last).to have_content "卒業課題"
+      end
+    end
+    context '期限目安順に並び替えボタンを押した場合' do
+      it '期限目安が早いタスクが一番上に表示される', :retry => 3  do 
+        visit pair_path(pair)
+        click_on "期限目安順に並び替え"
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content "テストタイトル"
+      end
+    end
+    context '進行度順に並び替えボタンを押した場合' do
+      it '進行度が遅いタスクが一番上に表示される', :retry => 3 do
+        visit pair_path(pair)
+        click_on "進行度順に並び替え"
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content "衣装"
       end
     end
   end
