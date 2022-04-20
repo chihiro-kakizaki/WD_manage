43303ddabde4d6da6abdaef6dc76dba3b12f2fa6
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-21 08:05:12 +0900

    improve:session画面のエラー解除のためルーティング追加#24

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
+    get '/users', to: 'devise/registrations#new'
   end
 
   resources :users, only: %i[show] do
