36c480e21272ebd4e0386adfb48fa6c7a84109c2
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-22 14:51:11 +0900

    fix:expired_on,statusカラムにnul制約を追加しテストが通らなくなったため、テスト修正#7

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,29 +3,34 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   let!(:user) {FactoryBot.create(:user)}
   let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
-  let!(:assign) {FactoryBot.create(:assign, user_id: user.id, pair_id: pair.id)}
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションに引っかかる' do
-        task = Task.new(title: '', description: '失敗テスト')
+        task = Task.new(title: '', description: '失敗テスト', expired_on: '2022-10-10', status: 0, user: user, pair: pair)
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルが150字以上の場合' do
       it 'バリデーションに引っかかる' do
-        task = Task.new(title: 'a' * 151 , description: '失敗テスト')
+        task = Task.new(title: 'a' * 151 , description: '失敗テスト', expired_on: '2022-10-10', status: 0, user: user, pair: pair)
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションに引っかかる' do
-        task = Task.new(title: '失敗テスト', description:'')
+        task = Task.new(title: '失敗テスト', description:'', expired_on: '2022-10-10', status: 0, user: user, pair: pair )
+        expect(task).not_to  be_valid
+      end
+    end
+    context 'タスクの期日目安が空の場合' do
+      it 'バリデーションに引っかかる' do
+        task = Task.new(title: '失敗テスト', description:'失敗テスト', expired_on: '', status: 0, user: user, pair: pair )
         expect(task).not_to  be_valid
       end
     end
     context 'タスクのタイトルと詳細が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', description: '成功テスト', user: user, pair: pair)
+        task = Task.new(title: '成功テスト', description: '成功テスト', expired_on: '2022-10-10', status: 0, user: user, pair: pair)
         expect(task).to be_valid
       end
     end
