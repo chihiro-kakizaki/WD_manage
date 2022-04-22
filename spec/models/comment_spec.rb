c251f47595f44bf5cadf6f9960d26a7c03ee9bec
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-22 14:51:32 +0900

    add:コメントモデルのテスト追加#9

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) {FactoryBot.create(:user)}
+  let!(:pair) {FactoryBot.create(:pair, owner_id: user.id )}
+  let!(:task) {FactoryBot.create(:task, user_id: user.id, pair_id: pair.id)}
+  describe 'バリデーションのテスト' do
+    context 'コメントが空の場合' do
+      it 'バリデーションに引っかかる' do
+        comment = Comment.new(content: '', user: user, task: task)
+        expect(comment).not_to be_valid
+      end
+    end
+    context 'コメントの内容が100文字以上の場合' do
+      it 'バリデーションに引っかかる' do
+        comment = Comment.new(content: 'a'* 101, user: user, task: task)
+        expect(comment).not_to be_valid
+      end
+    end
+    context 'コメントの内容が1文字以上100文字以下の場合' do
+      it 'バリデーションが通る' do
+        comment = Comment.new(content: '成功テスト', user: user, task: task)
+        expect(comment).to be_valid
+      end
+    end
+  end
 end
