4499bbe767917bff44b65d2ee91670b69a6c8c48
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-23 14:32:08 +0900

    add:seedデータ作成#30

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -11,7 +11,7 @@ class Pair < ApplicationRecord
                 }
 
   has_many :assigns, dependent: :destroy
-  has_many :users, through: :assigns 
+  has_many :users, through: :assigns
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   
   has_many :tasks, dependent: :destroy
