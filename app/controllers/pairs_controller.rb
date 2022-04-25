a77740f2124a87d4ca725e3d7a02446533cfcbe4
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-25 22:02:58 +0900

    improve:pair作成時のseasonをコントローラにて決定するように設定#6

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -11,6 +11,16 @@ class PairsController < ApplicationController
     @pair = current_user.build_pair(pair_params)
     @pertner_email = params[:pertner_email]
     pertner = User.find_by(email: params[:pertner_email])
+    case @pair.weddingday_on.month
+    when 3..5
+      @pair.season = 0
+    when 6..8
+      @pair.season = 1
+    when 9..11
+      @pair.season = 2
+    when 1 || 2 || 12
+      @pair.season = 3
+    end
     if pertner.nil? || pertner == current_user || (pertner && current_user).assign.present? 
       flash.now[:danger] = "メールアドレスが正しくありません"
       render :new
@@ -31,6 +41,16 @@ class PairsController < ApplicationController
 
   def update
     if pair_params[:weddingday_on].present?
+      case @pair.weddingday_on.month
+      when 3..5
+        pair_params[:season] = 0
+      when 6..8
+        pair_params[:season] = 1
+      when 9..11
+        pair_params[:season] = 2
+      when 1 || 2 || 12
+        pair_params[:season] = 3
+      end
       @pair.update(pair_params)
       redirect_to pair_path(@pair)
     else
