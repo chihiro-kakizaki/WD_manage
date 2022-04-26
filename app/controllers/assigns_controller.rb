949fa872ede51cc507ba97c89e38f6140d1b4f5d
chihiro-kakizaki <kkzkchr@gmail.com>
2022-04-26 09:40:14 +0900

    fix:ペア登録時に挙式季節を同時に登録できるようにmodelにて定義#6

---
+++ app/controllers/assigns_controller.rb
@@ -0,0 +1,8 @@
+class AssignsController < ApplicationController
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -11,21 +11,8 @@ class PairsController < ApplicationController
     @pair = current_user.build_pair(pair_params)
     @pertner_email = params[:pertner_email]
     pertner = User.find_by(email: params[:pertner_email])
-    case @pair.weddingday_on.month
-    when 3..5
-      @pair.season = 0
-    when 6..8
-      @pair.season = 1
-    when 9..11
-      @pair.season = 2
-    when 1 || 2 || 12
-      @pair.season = 3
-    end
     if pertner.nil? || pertner == current_user || (pertner && current_user).assign.present? 
       flash.now[:danger] = "メールアドレスが正しくありません"
-      render :new
-    elsif @pair.weddingday_on.nil?
-      flash.now[:danger] = "挙式日を入力してください"
       render :new
     elsif @pair.save
       @pair.assigns.create(user: pertner)
@@ -40,21 +27,9 @@ class PairsController < ApplicationController
   end
 
   def update
-    if pair_params[:weddingday_on].present?
-      case @pair.weddingday_on.month
-      when 3..5
-        pair_params[:season] = 0
-      when 6..8
-        pair_params[:season] = 1
-      when 9..11
-        pair_params[:season] = 2
-      when 1 || 2 || 12
-        pair_params[:season] = 3
-      end
-      @pair.update(pair_params)
+    if  @pair.update(pair_params)
       redirect_to pair_path(@pair)
     else
-      flash.now[:danger] = "挙式日を入力してください"
       render :edit
     end
   end
